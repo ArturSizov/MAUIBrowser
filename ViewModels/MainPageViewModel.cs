0da0d96c263ff567cab8ac38bb0bb4ccb2a1cfd6
Artur Sizov <artur-sa@mail.ru>
2023-08-17 14:17:43 +0300

    Установлен Prism.Core

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
 using UraniumUI;
+using Prism.Mvvm;
+using Prism.Commands;
 
 namespace MAUIBrowser.ViewModels;
-public class MainPageViewModel : BindableObject
+public class MainPageViewModel : BindableBase
 {
 
 	#region Public property 
 	public string Title => "MAUI Browser";
-	#endregion
+    #endregion
+
+
+    #region Commands 
+    public ICommand BackCommand => new DelegateCommand(async () =>
+    {
+        await Shell.Current.Navigation.PopAsync();
+    });
+
+    #endregion
 }
